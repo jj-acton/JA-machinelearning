9a97c746a374a3b10ed99f60cde914d0f097a443
jj-acton <actonjj@gmail.com>
2024-10-27 00:08:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ st.title('⚙️ Machine Learning App')
 
 st.info('This app builds a machine learning model!')
 
+
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
