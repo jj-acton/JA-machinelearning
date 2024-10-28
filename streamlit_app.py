24b82ea9586802d6fa7b21212b1d49fd7b75ede2
jj-acton <actonjj@gmail.com>
2024-10-28 22:33:09 +0000

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.expander("Data"):
 with st.expander("Data visualisation"):
   st.scatter_chart(
     data=df,
-    x="bill_lenght_mm",
+    x="bill_length_mm",
     y="body_mass_g",
     color="species"
   )
