a886763f9a091fa84e1956c4899a3f30c929353e
jj-acton <actonjj@gmail.com>
2024-10-29 15:30:38 +0000

    progress column

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,3 +96,8 @@ st.subheader("Predicted Species")
 df_prediction_probability
 penguin_species = np.array(["Adelie", "Chinstrap", "Gentoo"])
 st.success(penguin_species[prediction][0])
+
+# Display Progress Column
+
+st.data_editor(input_row,
+              column_config = {"Probability": st.coulumn_config.ProgressColumn("Penguin Probability", max_value = 1)})
